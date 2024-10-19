0cadd1706d5b99c4b479736e12120e86d067f1dc
Erick-bonfati <erickbonfati39@gmail.com>
2024-10-19 17:13:24 -0300

    Iniciando criacao de formulario

---
+++ 1/script.js
@@ -0,0 +1,5 @@
+// Seleciona o elemento com a classe "say-hi"
+const olaMundo = document.querySelector(".say-hi");
+
+// Define o conteúdo HTML do elemento selecionado
+olaMundo.innerHTML = "Hello World";
